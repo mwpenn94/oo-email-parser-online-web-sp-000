8cfc181c09f1209ace5c95ea6986dcf524147449
Michael Penn <mwpenn94@gmail.com>
2021-02-15 23:13:06 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,28 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+def self.new_from_csv(csv_data)
+    # Split the CSV data into an array of individual rows.
+    rows = csv_data.split("\n")
+    # For each row, let's collect a Person instance based on the data
+    people = rows.collect do |row|
+      # Split the row into 3 parts, name, age, company, at the ", "
+      data = row.split(", ")
+      name = data[0]
+      age = data[1]
+      company = data[2]
+ 
+      # Make a new instance
+      person = self.new # self refers to the Person class. This is Person.new
+      # Set the properties on the person.
+      person.name = name
+      person.age = age
+      person.company = company
+      # Return the person to collect
+      person
+    end
+    # Return the array of newly created people.
+    people
+  end
+end
