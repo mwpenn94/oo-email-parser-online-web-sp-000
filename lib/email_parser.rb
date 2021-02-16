301eba09932c044f6c58d0a4c434ac456d9537b4
Michael Penn <mwpenn94@gmail.com>
2021-02-16 02:24:54 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,7 +7,7 @@ class EmailAddressParser
   attr_accessor :email_addresses
  
   def self.parse(csv_data)
-    rows = csv_data.split("\n")
+    rows = csv_data.split()
     # For each row, let's collect a Person instance based on the data
     people = rows.collect do |row|
       # Split the row into 3 parts, name, age, company, at the ", "
